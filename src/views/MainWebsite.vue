6d523f116547c8afdbb5e2a365ae814cb7ffff32
Shazz250 <udohsharon12@gmail.com>
2024-07-18 22:22:57 +0300

    latest update

--- src/views/MainWebsite.vue
+++ src/views/MainWebsite.vue
@@ -1,29 +1,149 @@
 <template>
+    <!-- Top View -->
     <div>
-
-        <section class=" bg-teal-500 w-full">
-    <div class="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
+        <section class=" bg-teal-400 w-full">
+    <div class="container mx-auto py-16 px-6 sm:px-6 lg:px-8">
         <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
             <div class="max-w-lg">
-                <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">About Us</h2>
-                <p class="mt-4 text-gray-600 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis
-                    eros at lacus feugiat hendrerit sed ut tortor. Suspendisse et magna quis elit efficitur consequat.
-                    Mauris eleifend velit a pretium iaculis. Donec sagittis velit et magna euismod, vel aliquet nulla
-                    malesuada. Nunc pharetra massa lectus, a fermentum arcu volutpat vel.</p>
+                <span class="text-blue-700 font-medium">KenyanNurse</span>
+                <h2 class="text-3xl mt-4 font-bold text-red-600 sm:text-3xl">About Us</h2>
+                <p class="mt-4 text-black text-lg">Welcome to Innovatech Solutions, where we transform your ideas into reality. As a leading provider of cutting-edge technological solutions,
+                     we specialize in delivering innovative products and services that drive success for businesses of all sizes. Our team of dedicated professionals is committed to excellence,
+                      leveraging the latest advancements in technology
+                     to create customized solutions tailored to your unique needs. At Innovatech Solutions, 
+                    we believe in fostering strong partnerships and delivering exceptional value 
+                    to help you stay ahead in a rapidly evolving digital landscape.</p>
+
+                    <!-- Search Bar -->
                 <div class="mt-8">
-                    <a href="#" class="text-blue-500 hover:text-blue-600 font-medium">Learn more about us
-                        <span class="ml-2">&#8594;</span></a>
+
+                    <div class="flex relative rounded-md w-full px-4 max-w-xl">
+                        <input type="text" name="q" id="query" placeholder="Button, Footer, etc"
+                            class="w-full p-3 rounded-md border-2 border-r-white rounded-r-none border-gray-300 placeholder-gray-500 dark:placeholder-gray-300 dark:bg-gray-500dark:text-gray-300 dark:border-none " />
+                        <button
+                            class="inline-flex items-center gap-2 bg-violet-700 text-white text-lg font-semibold py-3 px-6 rounded-r-md">
+                            <span>search</span>
+                            <span class="hidden md:block">
+                                <svg class="text-gray-200 h-5 w-5 p-0 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                    xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px"
+                                    viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
+                                    width="512px" height="512px">
+                                    <path
+                                        d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                                </svg>
+                            </span>
+                        </button>
+                    </div>
+                    
                 </div>
             </div>
-            <div class="mt-12 md:mt-0">
+            <!-- Image -->
+            <!-- <div class="mt-12 md:mt-0">
                 <img src="https://images.unsplash.com/photo-1531973576160-7125cd663d86" alt="About Us Image" class="object-cover rounded-lg shadow-md">
-            </div>
+            </div> -->
+
+            <div class="mt-12 md:mt-0 relative w-full h-96">
+            <img 
+            :src="currentImage" 
+            alt="Slideshow Image" 
+            class="object-cover rounded-lg shadow-md w-full h-full"
+            >
+  </div>
+
+
+
         </div>
     </div>
 </section>
   
 
     </div>
+
+    <!-- Our Services -->
+     <div class=" bg-white w-full h-full">
+        <h2 class="text-3xl font-bold text-teal-800  mt-6 text-center">Our Services</h2>
+        <h2 class=" text-gray-800  mt-6 text-center">Browse through our catalogue of services and find what you're looking for.</h2>
+
+        
+    <section class="text-gray-700 body-font mt-3">
+    <div class="container py-4 mx-auto">
+        <div class="flex flex-wrap text-center justify-center">
+            <div class="p-4 md:w-1/4 sm:w-1/2">
+                <div class="px-4 py-6 transform transition duration-500 hover:scale-110">
+                    <div class="flex justify-center">
+                        <img src="https://image3.jdomni.in/banner/13062021/58/97/7C/E53960D1295621EFCB5B13F335_1623567851299.png?output-format=webp" class="w-32 mb-3">
+                    </div>
+                    <h2 class="title-font font-medium text-teal-800">Latest Milling Machinery</h2>
+                </div>
+            </div>
+
+            <div class="p-4 md:w-1/4 sm:w-1/2">
+                <div class="px-4 py-6 transform transition duration-500 hover:scale-110">
+                    <div class="flex justify-center">
+                        <img src="https://image2.jdomni.in/banner/13062021/3E/57/E8/1D6E23DD7E12571705CAC761E7_1623567977295.png?output-format=webp" class="w-32 mb-3">
+                    </div>
+                    <h2 class="title-font font-medium text-teal-800">Reasonable Rates</h2>
+                </div>
+            </div>
+
+            <div class="p-4 md:w-1/4 sm:w-1/2">
+                <div class="px-4 py-6 transform transition duration-500 hover:scale-110">
+                    <div class="flex justify-center">
+                        <img src="https://image3.jdomni.in/banner/13062021/16/7E/7E/5A9920439E52EF309F27B43EEB_1623568010437.png?output-format=webp" class="w-32 mb-3">
+                    </div>
+                    <h2 class="title-font font-medium text-teal-800">Time Efficiency</h2>
+                </div>
+            </div>
+
+            <div class="p-4 md:w-1/4 sm:w-1/2">
+                <div class="px-4 py-6 transform transition duration-500 hover:scale-110">
+                    <div class="flex justify-center">
+                        <img src="https://image3.jdomni.in/banner/13062021/58/97/7C/E53960D1295621EFCB5B13F335_1623567851299.png?output-format=webp" class="w-32 mb-3">
+                    </div>
+                    <h2 class="title-font font-medium text-teal-800">Latest Milling Machinery</h2>
+                </div>
+            </div>
+
+            <div class="p-4 md:w-1/4 sm:w-1/2">
+                <div class="px-4 py-6 transform transition duration-500 hover:scale-110">
+                    <div class="flex justify-center">
+                        <img src="https://image3.jdomni.in/banner/13062021/EB/99/EE/8B46027500E987A5142ECC1CE1_1623567959360.png?output-format=webp" class="w-32 mb-3">
+                    </div>
+                    <h2 class="title-font font-medium text-teal-800">Expertise in Industry</h2>
+                </div>
+            </div>
+
+            <div class="p-4 md:w-1/4 sm:w-1/2">
+                <div class="px-4 py-6 transform transition duration-500 hover:scale-110">
+                    <div class="flex justify-center">
+                        <img src="https://image2.jdomni.in/banner/13062021/3E/57/E8/1D6E23DD7E12571705CAC761E7_1623567977295.png?output-format=webp" class="w-32 mb-3">
+                    </div>
+                    <h2 class="title-font font-medium text-teal-800">Reasonable Rates</h2>
+                </div>
+            </div>
+
+            <div class="p-4 md:w-1/4 sm:w-1/2">
+                <div class="px-4 py-6 transform transition duration-500 hover:scale-110">
+                    <div class="flex justify-center">
+                        <img src="https://image3.jdomni.in/banner/13062021/58/97/7C/E53960D1295621EFCB5B13F335_1623567851299.png?output-format=webp" class="w-32 mb-3">
+                    </div>
+                    <h2 class="title-font font-medium text-teal-800">Latest Milling Machinery</h2>
+                </div>
+            </div>
+
+            <div class="p-4 md:w-1/4 sm:w-1/2">
+                <div class="px-4 py-6 transform transition duration-500 hover:scale-110">
+                    <div class="flex justify-center">
+                        <img src="https://image2.jdomni.in/banner/13062021/3E/57/E8/1D6E23DD7E12571705CAC761E7_1623567977295.png?output-format=webp" class="w-32 mb-3">
+                    </div>
+                    <h2 class="title-font font-medium text-teal-800">Reasonable Rates</h2>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+     </div>
 </template>
 
 <script>
@@ -32,7 +152,34 @@ export default {
         
 
         return {}
-    }
+    },
+    data() {
+    return {
+      images: [
+        "https://images.unsplash.com/photo-1531973576160-7125cd663d86",
+        "https://media.istockphoto.com/id/1265641298/photo/fried-papad.jpg?s=612x612&w=0&k=20&c=e_iEy4CTvU6Thn02zGgKt_TiSYAheCKmgfTF5j52ovU=",
+        "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw1fHxuYXR1cmV8ZW58MHwwfHx8MTY5NDA5OTcyOXww&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://image1.jdomni.in/banner/13062021/0A/52/CC/1AF5FC422867D96E06C4B7BD69_1623557926542.png"
+      ],
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    currentImage() {
+      return this.images[this.currentIndex];
+    },
+  },
+  methods: {
+    nextImage() {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    },
+  },
+  mounted() {
+    this.interval = setInterval(this.nextImage, 3000); // Change image every 3 seconds
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
 }
 </script>
 
